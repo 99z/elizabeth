a9ff2bcbf904ec1dc1d2f3e7e3dad8ef0972ae01
Nicolas S. Montanaro <mail@nicolasmontanaro.com>
2020-08-09 23:06:44 -0400

    this html is awful

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,9 @@ use colored::*;
 #[derive(Debug, Clone)]
 struct PageParseError;
 
+#[derive(Debug, Clone)]
+struct NoShadowError;
+
 impl fmt::Display for PageParseError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "table data not found")
@@ -18,6 +21,14 @@ impl fmt::Display for PageParseError {
 }
 
 impl std::error::Error for PageParseError {}
+
+impl fmt::Display for NoShadowError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "no shadow specified or no matching variant found")
+    }
+}
+
+impl std::error::Error for NoShadowError {}
 
 #[derive(Deserialize, Debug)]
 struct PageMeta {
@@ -34,11 +45,19 @@ struct Page {
     content: String
 }
 
+#[derive(PartialEq)]
+enum Games {
+    P3J,
+    P3A,
+    P4,
+    P4G
+}
+
 const P3_SELECTOR: &str = "#Persona_3_2";
 const P4_SELECTOR: &str = "#Persona_4_2";
 
 fn gen_table_selector(index: &usize) -> Selector {
-    let sel_string = format!("div:nth-child({}) > table > tbody > tr > td > table:nth-child(1) > tbody > tr > td > table:nth-child(2)", index);
+    let sel_string = format!("div:nth-child({}) > table > tbody > tr > td > table:nth-child(1) > tbody > tr > td > table:nth-child(2)", index + 1);
     Selector::parse(sel_string.as_str()).unwrap()
 }
 
@@ -51,19 +70,35 @@ fn get_page(page_id: &i32) -> Result<Html, Box<dyn Error>> {
     Ok(document)
 }
 
-fn get_table_node(doc: &Html) -> Result<Html, Box<dyn Error>> {
+fn get_table_node(doc: &Html, variant: &String) -> Result<Html, Box<dyn Error>> {
     let tabs = Selector::parse(".tabbertab").unwrap();
-    let tab_idx = doc.select(&tabs).position(|t| t.value().attr("title").unwrap() == "Normal Encounter").unwrap();
-    let tab_selector = gen_table_selector(&tab_idx);
+    let tab_selector = match doc.select(&tabs).position(|t| t.value().attr("title").unwrap() == variant) {
+        Some(idx) => gen_table_selector(&idx),
+        None => Selector::parse("table > tbody > tr > td > table:nth-child(1) > tbody > tr > td > table:nth-child(2)").unwrap()
+    };
     let table_node = doc.select(&tab_selector);
 
     let resistance_table = Html::parse_fragment(table_node.map(|n| n.html()).collect::<String>().as_str());
     Ok(resistance_table)
 }
 
-fn get_game_section(page: &Html) -> Result<Html, Box<dyn Error>> {
-    let selector = Selector::parse(format!("{} + .tabber", P3_SELECTOR).as_str()).unwrap();
-    let subsection_sel = page.select(&selector);
+fn get_game_section(page: &Html, game: Games) -> Result<Html, Box<dyn Error>> {
+    let persona_selector = if game == Games::P3J {
+        P3_SELECTOR
+    } else {
+        P4_SELECTOR
+    };
+
+    let base_selector = Selector::parse(format!("{} + .tabber", persona_selector).as_str()).unwrap();
+    let mut subsection_sel = page.select(&base_selector);
+
+    let mut size = 0;
+    for _ in subsection_sel {
+        size += 1;
+    }
+
+    let selector = if size == 0 { Selector::parse(format!("{} + table", persona_selector).as_str()).unwrap() } else { base_selector.clone() };
+    subsection_sel =  page.select(&selector);
     let subsection = Html::parse_fragment(subsection_sel.map(|n| n.html()).collect::<String>().as_str());
 
     Ok(subsection)
@@ -143,8 +178,27 @@ fn print_resistances(table: &HashMap<String, Vec<String>>) {
                 }
                 println!();
             },
-            _ => { println!("") }
+            _ => { }
         }
+    }
+}
+
+fn determine_game(game: &String) -> Games {
+    match game.to_lowercase().as_str() {
+        "3" => Games::P3J,
+        "3j" => Games::P3J,
+        "3a" => Games::P3A,
+        "4" => Games::P4,
+        "4g" => Games::P4G,
+        _ => Games::P3J
+    }
+}
+
+fn normalize_variant(variant: &str) -> String {
+    match variant {
+        "normal" => "Normal Encounter".to_string(),
+        "sub" => "Sub-boss".to_string(),
+        _ => "Normal Encounter".to_string()
     }
 }
 
@@ -152,15 +206,31 @@ fn main() -> Result<(), Box<dyn Error>>{
     let args: Vec<String> = args().collect();
     let mut opts = getopts::Options::new();
     opts.reqopt("s", "shadow", "Name of shadow", "SHADOW")
-        .reqopt("p", "persona", "Series number", "PERSONA");
+        .reqopt("p", "persona", "Series number", "PERSONA")
+        .optopt("v", "variant", "Enemy variant", "VARIANT");
 
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => { m },
         Err(f) => { panic!(f.to_string()) }
     };
 
-    let persona = matches.opt_str("p").unwrap();
-    let shadow = matches.opt_str("s").unwrap();
+    let persona = match matches.opt_str("p") {
+        Some(p) => { p },
+        None => { "p3".to_string() }
+    };
+    let shadow = match matches.opt_str("s") {
+        Some(s) => { s },
+        None => {
+            eprintln!("Must specify shadow name.");
+            return Err(NoShadowError.into())
+        }
+    };
+    let variant = match matches.opt_str("v") {
+        Some(v) => { normalize_variant(&v) },
+        None => { "Normal Encounter".to_string() }
+    };
+
+    let game = determine_game(&persona);
 
     let page_id = match get_page_id(&persona, &shadow) {
         Ok(id) => { id },
@@ -177,19 +247,25 @@ fn main() -> Result<(), Box<dyn Error>>{
         Err(e) => { panic!(e.to_string()) }
     };
 
-    let subsection = match get_game_section(&page) {
+    let subsection = match get_game_section(&page, game) {
         Ok(s) => { s },
         Err(e) => { panic!(e.to_string()) }
     };
 
-    let table_node = match get_table_node(&subsection) {
-        Ok(t) => { t },
-        Err(e) => { panic!(e.to_string()) }
+    let table_node = match get_table_node(&subsection, &variant) {
+        Ok(t) => t,
+        Err(e) => {
+            eprintln!("{}", e.to_string());
+            std::process::exit(1);
+        }
     };
 
     let table_data = match extract_table_data(table_node) {
         Ok(t) => { t },
-        Err(e) => { panic!(e.to_string()) }
+        Err(_) => {
+            eprintln!("found table, but failed to parse it. Maybe HTML changed?");
+            std::process::exit(1);
+        }
     };
 
     print_resistances(&table_data);
