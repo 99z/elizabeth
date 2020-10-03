9a999432069cab612f22fb4c20d480a388a01109
Nicolas S. Montanaro <mail@nicolasmontanaro.com>
2020-10-03 16:50:30 -0400

    better error handling

--- src/errors/mod.rs
+++ src/errors/mod.rs
@@ -1,27 +1,23 @@
 use core::fmt;
 
 #[derive(Debug, Clone)]
-pub struct NoShadowError;
+pub struct NoShadowError {
+    pub name: String,
+    pub game: String,
+}
 
 #[derive(Debug, Clone)]
-pub struct SelectorParseError;
-
-#[derive(Debug, Clone)]
-pub struct NoVariantError;
+pub struct NoVariantError {
+    pub shadow_name: String,
+    pub game: String,
+    pub variant: Vec<String>,
+}
 
 impl std::error::Error for NoShadowError {}
 
 impl fmt::Display for NoShadowError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "no shadow found")
-    }
-}
-
-impl std::error::Error for SelectorParseError {}
-
-impl fmt::Display for SelectorParseError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "failed to parse CSS selector")
+        write!(f, "Shadow not found: {} for game: {}", self.name, self.game)
     }
 }
 
@@ -29,6 +25,6 @@ impl std::error::Error for NoVariantError {}
 
 impl fmt::Display for NoVariantError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "no shadow found matching game + variant combination")
+        write!(f, "Shadow {} not found in: {} of variant: {:#?}", self.shadow_name, self.game, self.variant)
     }
 }
